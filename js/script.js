7c45f30c0e32cc964df8f27f60b8f5dbd98ceb5f
fabio <fabio>
2024-06-19 19:53:30 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Esto escribe por consola: hola mundo
+console.log('hola mundo') 
+// Esto muestra una ventana emergente:
+alert('Hola mundo Emergente!')
+// Esto escribe en el html:
+document.write('Hola mundo en el documento')
